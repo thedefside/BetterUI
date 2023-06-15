78e9c2d1138421e4c2750f83f6c0744f601dc974
Janek <jan.waraszko@gmail.com>
2023-06-15 23:14:40 +0200

    Required updates for Valheim version 0.216.9

--- BetterUI/Main.cs
+++ BetterUI/Main.cs
@@ -21,7 +21,7 @@ namespace BetterUI
           MODNAME = "BetterUI",
           AUTHOR = "MK",
           GUID = AUTHOR + "_" + MODNAME,
-          VERSION = "2.4.2";
+          VERSION = "2.4.3";
 
         internal static ManualLogSource log;
         internal readonly Harmony harmony;
